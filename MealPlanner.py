55aee0a7f8ca64795d2bd4a56ed534dfd01523eb
Coranath <34037243+Coranath@users.noreply.github.com>
2023-10-26 22:14:35 -0700

    Initial commit

---
+++ MealPlanner.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+if __name__ == '__main__':
+
+    # TODO first time using the app setup profile 
+    # including how many people you intend to feed.
+
+    print('Welcome to the meal planner app!')
+    print('''Please choose one:
+          1. Enter a protein and get a meal plan''')
